3c79bc98723e6fed77335146f2b9a9d5be3dbb7d
RoodeyMental <43878858+RoodeyMental@users.noreply.github.com>
2018-10-27 18:53:46 +0200

    Update Privatekunde.java
    Corrected typo in classname

--- src/UML/Privatekunde.java
+++ src/UML/Privatekunde.java
@@ -1,6 +1,6 @@
 package UML;
 
-public class Privatekunde extends Kunde {
+public class Privatkunde extends Kunde {
     private String vorname;
     private String nachname;
     private Adresse postAdresse;
