f3136e8920147bcece0caa39557030f403e17610
Markus Wiesheu <wiesheu@hm.edu>
2018-11-29 19:28:40 +0100

    added private constructors to StringContainer and GameLogic to hide implicit ones, removed bock of comments

--- src/main/java/testyourbrain/GameLogic.java
+++ src/main/java/testyourbrain/GameLogic.java
@@ -1,10 +1,14 @@
 package testyourbrain;
 
 public class GameLogic {
+
     public static final boolean DEBUGMODE = false;
     private static GameDifficulty difficulty;
     private static GameCategory category;
     private static GameState gameState;
+
+    private GameLogic() {
+    }
 
     public static void setDifficulty(GameDifficulty difficulty) {
         GameLogic.difficulty = difficulty;
@@ -17,7 +21,6 @@ public class GameLogic {
     public static void setGameState(GameState gameState) {
         GameLogic.gameState = gameState;
     }
-
 
     public static GameDifficulty getDifficulty() {
         return difficulty;
--- src/main/java/testyourbrain/StringContainer.java
+++ src/main/java/testyourbrain/StringContainer.java
@@ -2,6 +2,8 @@ package testyourbrain;
 
 public class StringContainer {
 
+    private StringContainer() {
+    }
 
     public static final String WELCOME_MESSAGE = " Hallo, willkommen bei Test your Brain. Dem besten Skill auf diesem Planeten. ";
     public static final String GOOD_BYE_MESSAGE = "Tschuess, bis zum naechsten mal. Ich hoffe du hast was gelernt.";
@@ -14,12 +16,5 @@ public class StringContainer {
     public static final String RULES = "Der Text fuer die Regeln des Spiels kann von den Maedels dann hier eingegeben werden. Los gehts! ";
     public static final String START_MESSAGE = "Ok lass uns Anfangen, wenn ich dir eine neue Frage stellen soll, sage einfach \"naechste Frage\".";
 
-    /*
-    public static String WELCOME_MESSAGE = "<speak><p> Hallo, willkommen bei Test your Brain. <amazon:effect name=\"whispered\"> Dem besten Skill auf diesem Planeten.</amazon:effect></p></speak>";
-    public static String GOOD_BYE_MESSAGE = "<speak><p>Tschuess, bis zum naechsten mal. Ich hoffe du hast was gelernt.</p></speak>";
-    public static String SKILL_DESCRIPTION = "<speak><p>Test your Brain ist der Skill, um dein Allgemeinwissen zu verbessern. Waehle eine Schwierigkeit und eine Kategorie und lege einfach drauf los. Sage einfach: Alexa, lass uns anfangen.</p></speak>";
-    public static String UNKNOWN_MESSAGE = "<speak><p>Tut mir leid, das weiss ich nicht. Sage einfach Hilfe.</p></speak>";
-    public static String GO_AHEAD_MESSAGE = "<speak><p>Jetzt sollte ich die letzte Frage wiederholen. Oh habe ich das grade laut gesagt?</p></speak>";
-    public static String RULES_QUESTION = "<speak><p>Willst du die Regeln erklaert bekommen?</p></speak>";
-    */
+
 }
