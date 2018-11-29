6df09e3dc1845d3e681f9c72ec2bcb268fa6fa02
Markus Wiesheu <wiesheu@hm.edu>
2018-11-29 20:22:05 +0100

    edited Rules

--- src/main/java/testyourbrain/StringContainer.java
+++ src/main/java/testyourbrain/StringContainer.java
@@ -8,12 +8,12 @@ public class StringContainer {
     public static final String WELCOME_MESSAGE = " Hallo, willkommen bei Test your Brain. Dem besten Skill auf diesem Planeten. ";
     public static final String GOOD_BYE_MESSAGE = "Tschuess, bis zum naechsten mal. Ich hoffe du hast was gelernt.";
     public static final String SKILL_DESCRIPTION = "Test your Brain ist der Skill, um dein Allgemeinwissen zu verbessern. Waehle eine Schwierigkeit und eine Kategorie und lege einfach drauf los. Soll ich dir die Regeln erklaeren? ";
-    public static final String REQUEST_DIFFICULTY = "Nenne mir bitte die gewuensche Schwierigkeitsstufe.";
-    public static final String REQUEST_CATEGORY = "Nenne mir bitte die gewuensche Kategorie.";
+    public static final String REQUEST_DIFFICULTY = "Nenne mir bitte die gewuenschte Schwierigkeitsstufe.";
+    public static final String REQUEST_CATEGORY = "Nenne mir bitte die gewuenschte Kategorie.";
     public static final String UNKNOWN_MESSAGE = "Tut mir leid, das weiss ich nicht. Sage einfach Hilfe.";
     public static final String GO_AHEAD_MESSAGE = "Jetzt sollte ich die letzte Frage wiederholen. Oh habe ich das grade laut gesagt?";
     public static final String RULES_QUESTION = "Soll ich dir noch die Regeln erklaeren?";
-    public static final String RULES = "Im laufe einer Runde stelle ich dir 10 Fragen. Für jede richtig beantwortete Frage bekommst du Punkte, für leichte Fragen einen Punkt, mittelschwere Fragen 2 und für Schwere 3 . Damit du weisst, wie gut du abgeschnitten hast, gebe ich dir am Ende einer Runde deine erreichten Punkte aus. Alles klar ? Wunderbar, Los gehts! Kategorie und Schwierigkeitsstufe kannst du im Laufe des Spiels jederzeit ändern. ";
+    public static final String RULES = "Im laufe einer Runde stelle ich dir 10 Fragen. Für jede richtig beantwortete Frage bekommst du Punkte, für leichte Fragen einen Punkt, mittelschwere Fragen zwei und für Schwere drei. Damit du weisst, wie gut du abgeschnitten hast, gebe ich dir am Ende einer Runde deine erreichten Punkte aus. Alles klar ? Wunderbar, Los gehts! Kategorie und Schwierigkeitsstufe kannst du im Laufe des Spiels jederzeit ändern. ";
     public static final String START_MESSAGE = "Ok lass uns Anfangen, wenn ich dir eine neue Frage stellen soll, sage einfach \"naechste Frage\".";
 
 
