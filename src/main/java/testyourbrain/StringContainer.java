46bd8664381edab23439aae31db929db076a3f8c
Markus Wiesheu <wiesheu@hm.edu>
2018-11-29 13:53:40 +0100

    changed variables of StringContainer.java to final constants

--- src/main/java/testyourbrain/StringContainer.java
+++ src/main/java/testyourbrain/StringContainer.java
@@ -3,16 +3,16 @@ package main.java.testyourbrain;
 public class StringContainer {
 
 
-    public static String WELCOME_MESSAGE = " Hallo, willkommen bei Test your Brain. Dem besten Skill auf diesem Planeten. ";
-    public static String GOOD_BYE_MESSAGE = "Tschuess, bis zum naechsten mal. Ich hoffe du hast was gelernt.";
-    public static String SKILL_DESCRIPTION = "Test your Brain ist der Skill, um dein Allgemeinwissen zu verbessern. Waehle eine Schwierigkeit und eine Kategorie und lege einfach drauf los. Soll ich dir die Regeln erklaeren? ";
-    public static String REQUEST_DIFFICULTY = "Nenne mir bitte die gewuensche Schwierigkeitsstufe.";
-    public static String REQUEST_CATEGORY = "Nenne mir bitte die gewuensche Kategorie.";
-    public static String UNKNOWN_MESSAGE = "Tut mir leid, das weiss ich nicht. Sage einfach Hilfe.";
-    public static String GO_AHEAD_MESSAGE = "Jetzt sollte ich die letzte Frage wiederholen. Oh habe ich das grade laut gesagt?";
-    public static String RULES_QUESTION = "Soll ich dir noch die Regeln erklaeren?";
-    public static String RULES = "Der Text fuer die Regeln des Spiels kann von den Maedels dann hier eingegeben werden. Los gehts! ";
-    public static String START_MESSAGE = "Ok lass uns Anfangen, wenn ich dir eine neue Frage stellen soll, sage einfach \"naechste Frage\".";
+    public static final String WELCOME_MESSAGE = " Hallo, willkommen bei Test your Brain. Dem besten Skill auf diesem Planeten. ";
+    public static final String GOOD_BYE_MESSAGE = "Tschuess, bis zum naechsten mal. Ich hoffe du hast was gelernt.";
+    public static final String SKILL_DESCRIPTION = "Test your Brain ist der Skill, um dein Allgemeinwissen zu verbessern. Waehle eine Schwierigkeit und eine Kategorie und lege einfach drauf los. Soll ich dir die Regeln erklaeren? ";
+    public static final String REQUEST_DIFFICULTY = "Nenne mir bitte die gewuensche Schwierigkeitsstufe.";
+    public static final String REQUEST_CATEGORY = "Nenne mir bitte die gewuensche Kategorie.";
+    public static final String UNKNOWN_MESSAGE = "Tut mir leid, das weiss ich nicht. Sage einfach Hilfe.";
+    public static final String GO_AHEAD_MESSAGE = "Jetzt sollte ich die letzte Frage wiederholen. Oh habe ich das grade laut gesagt?";
+    public static final String RULES_QUESTION = "Soll ich dir noch die Regeln erklaeren?";
+    public static final String RULES = "Der Text fuer die Regeln des Spiels kann von den Maedels dann hier eingegeben werden. Los gehts! ";
+    public static final String START_MESSAGE = "Ok lass uns Anfangen, wenn ich dir eine neue Frage stellen soll, sage einfach \"naechste Frage\".";
 
     /*
     public static String WELCOME_MESSAGE = "<speak><p> Hallo, willkommen bei Test your Brain. <amazon:effect name=\"whispered\"> Dem besten Skill auf diesem Planeten.</amazon:effect></p></speak>";
