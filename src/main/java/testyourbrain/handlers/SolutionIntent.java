d06e251abbf79c09f96613f3e9d0c04ce640f8b6
Markus Wiesheu <wiesheu@hm.edu>
2019-01-03 14:50:33 +0100

    added answer possibilities

--- src/main/java/testyourbrain/handlers/SolutionIntent.java
+++ src/main/java/testyourbrain/handlers/SolutionIntent.java
@@ -26,7 +26,7 @@ public class SolutionIntent implements RequestHandler {
         if (GameLogic.getGameState() == GameState.ANSWER) {
             try {
                 Request request = handlerInput.getRequestEnvelope().getRequest();
-                String answer = ((IntentRequest) request).getIntent().getSlots().get("Solution").getValue().toLowerCase();
+                String answer = ((IntentRequest) request).getIntent().getSlots().get("ABC").getValue().toLowerCase();
                 response = checkAnswer(answer);
 
             } catch (Exception e) {
@@ -43,8 +43,19 @@ public class SolutionIntent implements RequestHandler {
     private String checkAnswer(String answer) {
         boolean result = false;
         String solution = GameLogic.getCurrentQuestion().getSolution();
+        int index = 0;
+        if (answer.equals("A") || answer.equals("a")) {
+            index = 0;
+        }
+        if (answer.equals("B") || answer.equals("b")) {
+            index = 1;
+        }
+        if (answer.equals("C") || answer.equals("c")) {
+            index = 2;
+        }
+        String userAnswer = GameLogic.getCurrentQuestion().getSuggestions().split(",")[index];
         for (String sutableOption : solution.split(",")) {
-            if (sutableOption.toLowerCase().equals(answer.toLowerCase())) {
+            if (sutableOption.toLowerCase().equals(userAnswer.toLowerCase())) {
                 result = true;
             }
         }
